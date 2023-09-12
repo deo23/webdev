aa2dd8e37a47f9ab2db9a6f1834e768407ae88fb
deo23 <muhammad.deo.tif421@polban.ac.id>
2023-09-12 19:04:27 +0700

    Adding Update and Delete Feature

--- Special Mission/CRUD-APP/client/src/App.js
+++ Special Mission/CRUD-APP/client/src/App.js
@@ -7,15 +7,38 @@ function App() {
 
   const [movieName, setMovieName] = useState('')
   const [review, setReview] = useState('')
+  const [movieReviewList, setMovieList] = useState([])
+  const [newReview, setNewReview] = useState('')
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response) => { 
+      setMovieList(response.data)
+    });
+  },[]);
 
   const submitReview = () => {
     Axios.post("http://localhost:3001/api/insert", {
       movieName: movieName,
-      movieReview: review
-    }).then(() => {
-      alert("Successful insert");
-    });
-  };
+      movieReview: review})
+    
+      setMovieList([...movieReviewList, {
+        movieName: movieName,
+        movieReview: review
+      }]);
+    };
+  
+    const deleteReview = (movie) => {
+      Axios.delete(`http://localhost:3001/api/delete/${movie}`);
+    };
+
+    const updateReview = (movie) => {
+      Axios.put("http://localhost:3001/api/update", {
+        movieName: movie,
+        movieReview: newReview
+      });
+      setNewReview("")
+    }
+
   return (
     <div className="App">
     <h1>CRUD APPLICATION</h1>
@@ -30,6 +53,23 @@ function App() {
         setReview(e.target.value)
       }} placeholder="Enter Review" />
       <button onClick={submitReview}>Submit</button>
+      {movieReviewList.map((val) => {
+        return (
+          <div className="card">
+            <h1>{val.movieName}</h1>
+            <p>{val.movieReview}</p>
+
+            <button onClick={() =>{deleteReview(val.movieName)}}>Delete</button>
+            <input type="text" id="updateInput" onChange={(e)=>{
+              setNewReview(e.target.value)
+            }} />
+            <button onClick={()=>{
+              updateReview(val.movieName)
+            }}>Update</button>
+          </div>
+        )
+        
+      })}
 
     </div>
     </div>
